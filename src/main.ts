5be435ca363b537dcb405de1f4ad06a2732e1a02
zaohon <1402767827@qq.com>
2026-06-01 10:33:21 +0800

    Serialize BigInt responses

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
